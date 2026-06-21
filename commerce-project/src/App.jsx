1eaecc4124da180b3a285c9e0cb4f9931be2806c
Arpan Shah <arpanshah102@gmail.com>
2026-06-20 23:01:35 -0400

    Day 3

---
+++ commerce-project/src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
+import { OrderPage } from './pages/OrderPage';
+import { TrackingPage } from './pages/TrackingPage';
+import { Page404 } from './pages/404';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="orders" element={<OrderPage />} />
+        <Route path="tracking" element={<TrackingPage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ commerce-project/src/pages/404.jsx
@@ -0,0 +1,21 @@
+import { Header } from '../components/Header';
+import { Link } from 'react-router';
+import './404.css';
+
+export function Page404() {
+  return (
+    <>
+      <title>Page Not Found</title>
+
+      <Header />
+
+      <div className="page-404">
+        <div className="error-code">404</div>
+        <div className="error-message">Page Not Found</div>
+        <Link to="/" className="back-to-home-link link-primary">
+          Back to Home
+        </Link>
+      </div>
+    </>
+  );
+}
